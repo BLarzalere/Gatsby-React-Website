28e3509aeb4a1188c99674db2fdee8f8400a1a89
Brent Larzalere <brent.larzalere@gmail.com>
2020-09-17 14:04:35 -0500

    Add files via upload

---
+++ public/component---src-pages-404-js-e0745268278ca9cea46d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),r=a.n(t),l=a("7oih");n.default=function(){return r.a.createElement(l.a,{fullMenu:!0},r.a.createElement("section",{id:"wrapper"},r.a.createElement("header",null,r.a.createElement("div",{className:"inner"},r.a.createElement("h2",null,"Oops ... something went awry!!"),r.a.createElement("p",null,"Try going back ",r.a.createElement("a",{href:"Studiofourfour7.com"},"here"))))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e0745268278ca9cea46d.js.map
