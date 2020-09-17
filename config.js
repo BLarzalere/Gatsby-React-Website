38a7a72d2f9fbe8f2d58dd7ed7a4861e5cfa70e9
Brent Larzalere <brent.larzalere@gmail.com>
2020-09-17 13:57:57 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteTitle: 'S447', // <title>
+  manifestName: 'SolidState',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  heading: 'S447',
+  subHeading: 'Custom Metal Art',  
+  socialLinks: [
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: '',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: '',
+    },
+  ]
+};
